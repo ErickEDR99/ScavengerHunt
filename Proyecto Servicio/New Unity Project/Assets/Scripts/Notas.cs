1aef2b644d27a67be27a1548d8b448729539971b
ErickEDR99 <erickedr99@gmail.com>
2022-10-22 14:08:16 -0500

    Sin musica

---
+++ Proyecto Servicio/New Unity Project/Assets/Scripts/Notas.cs
@@ -0,0 +1,45 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+
+public class Notas : MonoBehaviour
+{
+
+    [SerializeField] private VirtualButtonBehaviour virtualButton;
+    public UnityEvent OnButtonPressed;
+    public UnityEvent OnButtonReleased;
+
+
+    private void OnEnable()
+    {
+        virtualButton.RegisterOnButtonPressed(ButtonPressed);
+        virtualButton.RegisterOnButtonReleased(ButtonReleased);
+
+
+    }
+
+    public void OnDestroy()
+    {
+        virtualButton.UnregisterOnButtonPressed(ButtonPressed);
+        virtualButton.UnregisterOnButtonReleased(ButtonReleased);
+    }
+
+    private void ButtonPressed(VirtualButtonBehaviour button)
+    {
+        OnButtonPressed?.Invoke();
+        Debug.Log("Button Pressed");
+    }
+
+    private void ButtonReleased(VirtualButtonBehaviour button)
+    {
+        OnButtonReleased?.Invoke();
+        Debug.Log("Button Released");
+
+    }
+
+}
+*/
